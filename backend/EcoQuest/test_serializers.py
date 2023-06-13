9253fd6b807d51c945bb2fc7fd2520f4c2f7fb87
AlexDkj <alexandrosd13@gmail.com>
2023-06-13 14:53:59 +0300

    add tests

--- backend/EcoQuest/test_serializers.py
+++ backend/EcoQuest/test_serializers.py
@@ -13,19 +13,16 @@ from rest_framework.renderers import JSONRenderer
 
 # Create your tests here.
 
-@pytest.mark.usefixtures
 def test_poi_serializer() -> None:
     categoryid = Category(CategoryId=120,Name='nature')
     nomosid = N()
     perifereiaid = P()
     test = POIS.Meta.model(Name='John',CategoryId=categoryid,Longitude=12,Latitude=12)
-    #print(POIS(test).data)
     json = JSONRenderer().render(POIS(test).data)
-    exdata = {"Name":"John","CategoryId":120,"Longitude":"12.000000","Latitude":"12.000000",'CreatedDate': test.CreatedDate}
+    exdata = {"Name":"John","CategoryId":120,"Longitude":"12.000000","Latitude":"12.000000","CreatedDate": test.CreatedDate}
     jsonexd = JSONRenderer().render(exdata)
     assert json == jsonexd
 
-@pytest.mark.usefixtures
 def test_category_serializer() -> None:
     test = CS.Meta.model(CategoryId=120,Name='nature')
     json = JSONRenderer().render(CS(test).data)
