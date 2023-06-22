882ab2ce751f46e00e60ce9312555af459074e9b
Mike <sdi0800018@di.uoa.gr>
2023-06-22 23:49:26 +0300

    maps

---
+++ frontend/src/Types/PointOfInterest.ts
@@ -0,0 +1,11 @@
+// src/types.ts
+
+interface PointOfInterest {
+    name: string;
+    description: string;
+    longitude: number;
+    latitude: number;
+    image: string;
+    categoryId: number;
+}
+  
--- frontend/src/pages/PoiList.vue
+++ frontend/src/pages/PoiList.vue
@@ -1,10 +1,15 @@
 <template>
-<div>
+<div class="homepage">
   <h2>PoiList</h2>
   <div>
       <div v-for="poi in pois">
-        {{ poi.Name }} - {{poi.CategoryID}}
+        {{ poi.Name }} - {{poi.CategoryId}}
+        
       </div>
+     
+    </div>
+    <div class="map-container">
+      <Map :points-of-interest="mockPoints" key="map" />
     </div>
 </div>
 </template>
@@ -16,11 +21,42 @@ import InputText from 'primevue/inputtext';
 import MultiSelect from 'primevue/multiselect';
 import {useRouter} from "vue-router";
 import axios from "axios";
+import Map from "../components/common/Map.vue";
+import PointOfInterest from '../Types/PointOfInterest';
 
 interface Poi {
   name: string;
+  CategoryId: number;
   // Add other properties here if necessary
 }
+const mockPoints: PointOfInterest[] = [
+  {
+    name: 'Point of Interest 1',
+          description: 'Description 1',
+          longitude: 23.737539,
+          latitude: 37.983810,
+          image: 'https://fastly.picsum.photos/id/949/536/354.jpg?hmac=biBe6mOyyM3zjcsRQcyxfkHTNxHLyMzX2-x9rc-Ef8c',
+          categoryId: 1,
+        },
+        {
+          name: 'Point of Interest 2',
+          description: 'Description 2',
+          longitude: 23.787432,
+          latitude: 37.983720,
+          image: 'https://fastly.picsum.photos/id/886/200/200.jpg?hmac=pfmGQi7EpajLoJI0tKTPTUwOPQtH9YwE-wNl_kr7ErI',
+          categoryId: 2,
+        },
+        {
+          name: 'Point of Interest 3',
+          description: 'Description 3',
+          longitude: 23.287432,
+          latitude: 37.483720,
+          image: 'https://fastly.picsum.photos/id/886/200/200.jpg?hmac=pfmGQi7EpajLoJI0tKTPTUwOPQtH9YwE-wNl_kr7ErI',
+          categoryId: 2,
+        },
+]
+
+
 let pois: any = ref([]);
 const new_pois = ref<Poi[]>([]);
 
@@ -36,19 +72,27 @@ try {
   console.log(pois.value)
 })
 
-// watch(pois, () =>{
-//   pois.value.map((p: any) => {
-//     new_pois.value.push({
-//       name: p.Name
-//     })
-//   });
-//
-//   console.log(new_pois.value);
-// })
 
 </script>
 
 <style scoped lang="scss">
+.homepage{
+  display: grid;
+  grid-template-columns: 2fr 2fr; /* Adjust the column widths as needed */
+  gap: 20px; /* Adjust the gap between columns as needed */
+  height: 80vh; /* Adjust the height as needed */
+}
+
+
+.map-container {
+  width: 100%;
+  height: 100%;
+}
+.poi-list {
+  flex: 1;
+  padding: 16px;
+  overflow-y: auto;
+}
 
 .p-datatable{
   height: 100px;
