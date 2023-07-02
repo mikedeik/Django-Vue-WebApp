2fcd55fd12dd27172431d8986dab0666b4d00771
AlexDkj <alexandrosd13@gmail.com>
2023-07-02 14:40:50 +0300

    fix checks

--- backend/EcoQuest/tests_views.py
+++ backend/EcoQuest/tests_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
-from .views import PoIList, RegisterView, CreatePOIsAPIView, PoIDetails
 from .models import PointOfInterest, Category
+from .serializers import PointOfInterestSerializer as POIS
 from django.contrib.auth.models import User
 
 class TestPoiListView(TestCase):
@@ -8,20 +8,26 @@ class TestPoiListView(TestCase):
     def setUp(self) -> None:
         self.client = Client()
         categoryid = Category.objects.create(CategoryId=120,Name='nature')
-        [ PointOfInterest.objects.create(PointOfInterestId=12,Name='John',CategoryId=categoryid,Longitude=12,Latitude=12),
-          PointOfInterest.objects.create(PointOfInterestId=19,Name='Gianni',CategoryId=categoryid,Longitude=12,Latitude=12) ]
-    
+        PointOfInterest.objects.bulk_create([PointOfInterest(PointOfInterestId=12,Name='John',CategoryId=categoryid,Longitude=12,Latitude=12), 
+        PointOfInterest(PointOfInterestId=19,Name='Gianni',CategoryId=categoryid,Longitude=12,Latitude=12) ])
+
     def test_poilist_view(self):
+        data = PointOfInterest.objects.all()
+        testdata = POIS(data, many=True).data
         response = self.client.get("/ecoquest/poi/")
         self.assertEquals(response.status_code,200)
+        self.assertEquals(response.data,testdata)
+
 
 class TestPoiDetailsView(TestCase):
 
-    def setUp(self):
+    def setUp(self) -> None:
         self.client = Client()
         categoryid = Category.objects.create(CategoryId=120,Name='nature')
-        PointOfInterest.objects.create(PointOfInterestId=12,Name='John',CategoryId=categoryid,Longitude=12,Latitude=12)
+        self.data = PointOfInterest.objects.create(PointOfInterestId=12,Name='John',CategoryId=categoryid,Longitude=12,Latitude=12)
 
     def test_poidetails_view(self):
+        testdata = POIS(PointOfInterest.objects.get(PointOfInterestId=12)).data
         response = self.client.get("/ecoquest/poi/12/")
         self.assertEquals(response.status_code,200)
+        self.assertEquals(response.data,testdata)
