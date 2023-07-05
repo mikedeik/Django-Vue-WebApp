eb8616fbe3cc6e1eb3951689c44f55b696417aa5
AlexDkj <alexandrosd13@gmail.com>
2023-07-05 18:39:53 +0300

    tests run now with manytomany categories

--- backend/EcoQuest/tests_serializers.py
+++ backend/EcoQuest/tests_serializers.py
@@ -8,13 +8,15 @@ from .models import  Nomos as N, Perifereia as P, Category, SavedSearch as SE, P
 class TestPOISModelSerializer(TestCase):
 
     def setUp(self) -> None:
-        categoryid = Category.objects.create(CategoryId=120,Name='nature')
-        test = POIS.Meta.model.objects.create(Name='John',CategoryId=categoryid,Longitude=12,Latitude=12)
+        category1 = Category.objects.create(CategoryId=120,Name='natura')
+        category2 = Category.objects.create(CategoryId=10,Name='Δάση')
+        test = POIS.Meta.model.objects.create(PointOfInterestId=12,Name='John',Longitude=12,Latitude=12)
+        test.Categories.add(category1,category2)
 
     def test_serializer(self):
-        test = POIS.Meta.model.objects.get(CategoryId=120)
+        test = POIS.Meta.model.objects.get(PointOfInterestId=12)
         json = POIS(test).data
-        exdata = {"Name":"John","CategoryId":120,"Longitude":"12.000000","Latitude":"12.000000",'CreatedDate':POIS(test).data['CreatedDate']}
+        exdata = {"Name":"John","Categories":[10,120],"Longitude":"12.000000000000000","Latitude":"12.000000000000000",'CreatedDate':json['CreatedDate'],"KeyWords":""}
         jsonexd = exdata
         self.assertEquals(json,jsonexd)
 
@@ -34,8 +36,10 @@ class TestNotificationListModelSerializer(TestCase):
 
     def setUp(self) -> None:
         userid = User.objects.create(username="John",email="john@gmail.com")
-        categoryid = Category.objects.create(CategoryId=120,Name='nature')
-        poiid = POIS.Meta.model.objects.create(Name='John',CategoryId=categoryid,Longitude=12,Latitude=12)
+        category1 = Category.objects.create(CategoryId=120,Name='natura')
+        category2 = Category.objects.create(CategoryId=10,Name='Δάση')
+        poiid = POIS.Meta.model.objects.create(Name='John',Longitude=12,Latitude=12)
+        poiid.Categories.add(category1,category2)
         NLS.Meta.model.objects.create(NotificationId=10,PointOfInterestId=poiid,UserId=userid,Text="New notification for poi")
 
     def test_serializer(self):
