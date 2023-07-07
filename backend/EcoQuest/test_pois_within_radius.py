a2497822c791291ba421c4a98d68f287597e5f23
AlexDkj <alexandrosd13@gmail.com>
2023-07-07 12:19:07 +0300

    change assert check

--- backend/EcoQuest/test_pois_within_radius.py
+++ backend/EcoQuest/test_pois_within_radius.py
@@ -9,10 +9,10 @@ class Testpois_within_radius(TestCase):
     def setUp(self) -> None:
         category1 = Category.objects.create(CategoryId=120, Name='natura')
         category2 = Category.objects.create(CategoryId=10, Name='Δάση')
-        data = POIS.Meta.model.objects.create(PointOfInterestId=12, Name='John', Longitude=10, Latitude=1)
+        data = POIS.Meta.model.objects.create(PointOfInterestId=12, Name='John', Longitude=6, Latitude=1)
         data.Categories.add(category1,category2)
         userid = User.objects.create()
-        savedsearch = SavedSearch.objects.create(SavedSearchId=7, UserId=userid, Radius=9000)
+        savedsearch = SavedSearch.objects.create(SavedSearchId=7, UserId=userid, Radius=900)
         savedsearch.Categories.add(category1,category2)
 
     
@@ -20,4 +20,4 @@ class Testpois_within_radius(TestCase):
         pois = POIS.Meta.model.objects.all()
         savedsearch = SavedSearch.objects.get(SavedSearchId=7)
         testdata = pois_within_radius(self,pois,savedsearch)
-        self.assertNotEquals(testdata,None)
+        self.assertNotEquals(testdata,[])
